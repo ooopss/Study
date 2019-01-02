1766d7cd9c49cc05529e53dc1ff98873e35680b4
lanemori <jylia0205@gmail.com>
2019-01-02 06:28:48 +0300

    исправления после комментариев

--- курсач/курсач/Form1.Designer.cs
+++ курсач/курсач/Form1.Designer.cs
@@ -55,7 +55,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "  Начать игру";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
@@ -66,7 +66,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Добавить букву";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // label1
             // 
