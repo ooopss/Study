70a391b67ab6c5ba02c080f36c906de8a5e52d0d
lanemori <jylia0205@gmail.com>
2019-01-02 21:42:17 +0300

    добавлены тестовые тесты =)

---
+++ курсач/Charades.Tests/FirstUnitTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charades.Tests
+{
+    [TestFixture]
+    public class FirstUnitTests
+    {
+        [Test]
+        public void TestNumbaOne()
+        {
+            var a = 10;
+            var b = 5 * 2;
+
+            Assert.AreEqual(a, b);
+        }
+
+        [Test]
+        public void TestNumbaTwo()
+        {
+            var a = 10;
+            var b = 5 * 3;
+
+            Assert.AreNotEqual(a, b);
+        }
+
+        [Test]
+        public void TestThatGonnaFail()
+        {
+            var a = 30;
+            var b = 5 * 2;
+
+            Assert.AreEqual(a, b);
+        }
+    }
+}
