d7dd5333985bcd1bf778fb5b105258f3cc5c3932
lanemori <jylia0205@gmail.com>
2019-01-08 07:16:39 +0300

    реализован метод ButtonMakeAttemptClick, рефакторинг

--- Charades/AttemptResult.cs
+++ Charades/AttemptResult.cs
@@ -4,8 +4,19 @@ namespace курсач
 {
     public class AttemptResult
     {
+		/// <summary>
+		/// Является ли попытка угадать успешной
+		/// </summary>
         public bool IsSuccess { get; set; }
+
+		/// <summary>
+		/// Является ли игра завершенной (исчерпан лимит попыток)
+		/// </summary>
         public bool IsGameFailed { get; set; }
+
+		/// <summary>
+		/// Все позиции в слове, где находится угаданная буква (в случае, если попытка является успешной)
+		/// </summary>
         public IReadOnlyCollection<int> AllLetterPositions { get; set; }
     }
 }
--- Charades/Form1.cs
+++ Charades/Form1.cs
@@ -6,9 +6,11 @@ namespace курсач
 {
 	public partial class MainForm : Form
 	{
-		private char[] _letters;
 		private IGame _game;
-		private int imageIndex;
+
+		// TODO research можно ли реализовать INotifyPropertyChanged для _letters и _imageIndex
+		private char[] _letters;
+		private int _imageIndex;
 
 		public MainForm()
 		{
@@ -46,17 +48,66 @@ namespace курсач
 				_letters = new string('.', word.Length).ToCharArray();
 			}
 
+			_imageIndex = 0;
+			UpdateImage();
+			ShowLetters();
+		}
+
+		/// <summary>
+		/// Сделать попытку
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ButtonMakeAttemptClick(object sender, EventArgs e)
+		{
+			var text = TextBoxAttempt.Text.ToLower(); // преобразуем текстовый ввод в нижний регистр
+			if (text.Length == 0)
+			{
+				return;
+			}
+
+			var letter = text.ToCharArray()[0]; // первая буква
+
+			var result = _game.MakeAttempt(letter);
+
+			if (result.IsSuccess) // успешная попытка
+			{
+				// для каждой позиции из угаданных показать букву
+				foreach (var item in result.AllLetterPositions)
+				{
+					_letters[item] = letter;
+				}
+			}
+			else if (result.IsGameFailed) // game over
+			{
+				// показать полную виселицу и слово целиком
+				_imageIndex = 10;
+				_letters = _game.Word.ToCharArray();
+			}
+			else // попытка неудачная
+			{
+				// прибавить 1 к индексу текущего изображения
+				_imageIndex++;
+			}
+
+			UpdateImage();
+			ShowLetters();
+		}
+
+		private void ShowLetters()
+		{
 			LabelWord.Text = new string(_letters);
 		}
 
-		private void ButtonMakeAttemptClick(object sender, EventArgs e)
+		private void UpdateImage()
 		{
-
-		}
-
-		private void SetImage(int imageIndex)
-		{
-			pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("image" + imageIndex.ToString());
+			if (_imageIndex == 0)
+			{
+				pictureBox1.Image = null;
+				return;
+			}
+			pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager
+				.GetObject("image" + _imageIndex.ToString());
 		}
 	}
 }
