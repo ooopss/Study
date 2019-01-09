50e8a9a45cb136e83c612387187f6eed10cb57c4
lanemori <jylia0205@gmail.com>
2019-01-09 06:11:41 +0300

    #18 реализация класса LeadersManager

--- Charades/LeaderRecord.cs
+++ Charades/LeaderRecord.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace курсач
 {
-    class LeaderRecord
+	public class LeaderRecord
     {
         public string Name { get; set; }
         public int Score { get; set; }
--- Charades/LeadersManager.cs
+++ Charades/LeadersManager.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace курсач
 {
-    class LeadersManager : ILeadersManager
-    {
-        public IReadOnlyCollection<LeaderRecord> GetAllRecords()
-        {
-            throw new NotImplementedException();
-        }
+	public class LeadersManager : ILeadersManager
+	{
+		private const string _filePath = @"leaders.txt";
 
-        public void SaveLeaderResult(LeaderRecord newItem)
-        {
-            throw new NotImplementedException();
-        }
-        private const string _filePath = @"leaders.txt";
-    }
+		public IReadOnlyCollection<LeaderRecord> GetAllRecords()
+		{
+			var records = new List<LeaderRecord>();
+			var lines = File.ReadAllLines(_filePath);
+			foreach (var line in lines)
+			{
+				var items = line.Split(',');
+				var record = new LeaderRecord
+				{
+					Name = items[0],
+					Score = int.Parse(items[1]),
+					Date = DateTime.Parse(items[2])
+				};
+				records.Add(record);
+			}
+			return records.OrderByDescending(x => x.Score).Take(10).ToArray();
+		}
 
+
+		public void SaveLeaderResult(LeaderRecord newItem)
+		{
+			var newLine = $"{newItem.Name},{newItem.Score},{newItem.Date}";
+			File.AppendAllLines(_filePath, new[] { newLine });
+		}
+	}
 }
